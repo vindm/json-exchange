71ea6634bd85b5c90b8c7d9919f191052a3c2890
vindm <vindm@yandex-team.ru>
2016-10-07 17:08:13 +0300

    v0.0.2

--- examples/start.sh
+++ examples/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node bin/json-exchange.js -f 'examples/output.json' -t 'examples/input.json' -m 'keysets.*.keys.*.translations.ru' -r '\.ru$' -l '.en' -o 'examples/result.json'
+node bin/json-exchange -f 'examples/output.json' -t 'examples/input.json' -m 'keysets.*.keys.*.translations.ru' -r '\.ru$' -l '.en' -o 'examples/result.json'
